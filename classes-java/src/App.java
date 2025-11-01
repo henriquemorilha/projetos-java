b915fbfbf0ed31ac9ee7b34bed35bec9d95fd65d
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-01 19:50:21 -0300

    Classes Java: Instância VS Classe - Métodos

--- classes-java/src/App.java
+++ classes-java/src/App.java
@@ -1,4 +1,13 @@
 public class App {
     public static void main(String[] args) {
+        Usuario usuario = new Usuario(
+            "henrique.morilha", 
+            "123", 
+            "henrique.morilha@test.com"
+        );
+        System.out.println(usuario.email);
+        System.out.println(usuario.username);
+        System.out.println(usuario.password);
     }
+
 }
--- classes-java/src/Usuario.java
+++ classes-java/src/Usuario.java
@@ -4,6 +4,17 @@ public class Usuario {
     public String email;
     public boolean ativo;
 
+    public Usuario() {
+    
+    }
+        
+    public Usuario(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+
+    }
+
     public boolean isAtivo() {
         return ativo;
     }     
