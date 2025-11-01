20d0168cacab3d8e6ef2a9b158074e96779939d5
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-01 18:44:50 -0300

    Métodos Java: Parâmetros ou Argumentos do método

--- metodos-java/src/App.java
+++ metodos-java/src/App.java
@@ -5,9 +5,17 @@ public class App {
         alarmeHoraDoLanche();
         alarmeHoraDoLanche();
         System.out.println("Depois do Alarme");
+
+        calcularMedia(7.0, 9.0, 5.0);
+        calcularMedia(9.0, 1.0, 1.0);
     }
 
     public static void alarmeHoraDoLanche() {
         System.out.println("Hora do Lanche!");
+    }    
+    
+    public static void calcularMedia(double nota1, double nota2, double nota3) {
+        double media = (nota1 + nota2 + nota3) / 3.0;
+        System.out.println(media); 
     }    
 }
