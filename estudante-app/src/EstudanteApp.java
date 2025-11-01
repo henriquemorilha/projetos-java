cd9169a4a7b28ec3f5b70bba701591f5aefece4c
Henrique Morilha <henriquemorilha97@gmail.com>
2025-10-31 23:13:34 -0300

    Variáveis na prática em programas java

---
+++ estudante-app/src/EstudanteApp.java
@@ -0,0 +1,15 @@
+public class EstudanteApp {
+    public static void main(String[] args) {
+        int idade = 28;
+        char inicialDoNome = 'H';
+        char inicialDoSobrenome = 'M';
+        double media = 10.0;
+        boolean aprovado = true;
+
+        System.out.println(idade);
+        System.out.println(inicialDoNome);
+        System.out.println(inicialDoSobrenome);
+        System.out.println(media);
+        System.out.println(aprovado);
+    }
+}
