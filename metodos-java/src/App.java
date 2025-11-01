65e07b7dd9460fad21ad36cffd31d496a2ad0eff
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-01 18:52:41 -0300

    Métodos Java: Retorno do Método

--- metodos-java/src/App.java
+++ metodos-java/src/App.java
@@ -6,16 +6,21 @@ public class App {
         alarmeHoraDoLanche();
         System.out.println("Depois do Alarme");
 
-        calcularMedia(7.0, 9.0, 5.0);
-        calcularMedia(9.0, 1.0, 1.0);
+        double media = calcularMedia(7.0, 9.0, 5.0);
+        
+        if(media >= 7.0) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }    
     }
 
     public static void alarmeHoraDoLanche() {
         System.out.println("Hora do Lanche!");
     }    
     
-    public static void calcularMedia(double nota1, double nota2, double nota3) {
+    public static double calcularMedia(double nota1, double nota2, double nota3) {
         double media = (nota1 + nota2 + nota3) / 3.0;
-        System.out.println(media); 
+        return media;
     }    
 }
