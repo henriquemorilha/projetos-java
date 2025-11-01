310450b7a7b33129b891a8f829163607c3359b8f
Henrique Morilha <henriquemorilha97@gmail.com>
2025-10-31 23:35:06 -0300

    Criando uma String em Java

--- estudante-app/src/EstudanteApp.java
+++ estudante-app/src/EstudanteApp.java
@@ -5,11 +5,14 @@ public class EstudanteApp {
         char inicialDoSobrenome = 'M';
         double media = 10.0;
         boolean aprovado = true;
+        String nome = "Henrique Morilha";
 
         System.out.println(idade);
         System.out.println(inicialDoNome);
         System.out.println(inicialDoSobrenome);
         System.out.println(media);
         System.out.println(aprovado);
+        System.out.println(nome);
+        System.out.println("Henrique");
     }
 }
