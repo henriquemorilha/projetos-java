349fb6708c2a2ab4eac8313b24584a1df04fd467
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-03 19:31:10 -0300

    Variables: Experimenting with Primitive Variables

--- Employee/src/Employee.java
+++ Employee/src/Employee.java
@@ -7,5 +7,18 @@ public class Employee {
         boolean isAManager = true;
         System.out.println(isAManager);
         
+        double yearsOfService = 2.5;
+        System.out.println(yearsOfService);
+
+        int baseSalary = 3000;
+        System.out.println(baseSalary);
+
+        int overtimePayment = 40;
+    
+        int totalPayment = baseSalary + overtimePayment;
+        System.out.println(totalPayment);
+
+        double firstBonus = 10.00, secondBonus = 22.00, thirdBonus = 35.00;
+        System.out.println(firstBonus + secondBonus + thirdBonus);
     }
 }
