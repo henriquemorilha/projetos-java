55f90d537858bd8d29a72eca5fb83ef2d2bbf5b7
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-03 19:00:46 -0300

    Variables: Experimenting with Primitive Variables

---
+++ Employee/src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    public static void main(String[] args) {
+
+        int age = 28;
+        System.out.println(age);
+
+        boolean isAManager = true;
+        System.out.println(isAManager);
+        
+    }
+}
