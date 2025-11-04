45e96f2efa843e7384b227551ccf12fd99884e12
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-03 21:39:54 -0300

    Control Flux: Loops

---
+++ loops/src/ForLoops.java
@@ -0,0 +1,15 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++) {
+            System.out.println("I love for loops");
+        }
+
+        for(int i = 1; i <=10; i++) {
+            System.out.println(i);
+        }
+
+        for(int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
